924520f2355e092476423a62cce46836777f6406
MohamedElTaher1 <mohamedeltaher56@gmail.com>
2024-07-07 00:01:38 +0300

    Task

--- Technical-challenge/src/main/java/coreElements/FormField.java
+++ Technical-challenge/src/main/java/coreElements/FormField.java
@@ -3,27 +3,33 @@ package coreElements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
-public class FormField extends Element{
+public class FormField extends Element {
 
     public FormField(By locator) {
         super(locator);
     }
 
-    public void enterText(String text){
+    public void enterText(String text) {
         driver.element().type(elementLocator, text);
     }
-    public String getText(){
+
+    public String getText() {
         return driver.element().getText(elementLocator);
     }
-    public void validateCriticalField(String expectedValue){
+
+    public void validateCriticalField(String expectedValue) {
         driver.assertThat().element(elementLocator).text()
                 .isEqualTo(expectedValue).perform();
     }
-    public void validateNonCriticalField(String expectedValue){
+
+    public void validateNonCriticalField(String expectedValue) {
         driver.verifyThat().element(elementLocator).text()
                 .isEqualTo(expectedValue).perform();
     }
+
     public void ClearText() {
-        driver.element().keyPress(elementLocator, Keys.valueOf(Keys.chord(Keys.LEFT_ALT, "a", Keys.DELETE)));
+        for (char s : driver.element().getText(elementLocator).toCharArray()) {
+            driver.element().keyPress(elementLocator, Keys.BACK_SPACE);
+        }
     }
 }
